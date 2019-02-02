e27a081e4efb43f52b824f94e364b447b7c82c68
sybersecho <sybers_echo@yahoo.co.id>
2019-02-02 17:13:39 +0700

    create relations between supplier and product. closes #19

--- src/main/java/com/toko/maju/domains/v1/Supplier.java
+++ src/main/java/com/toko/maju/domains/v1/Supplier.java
@@ -1,7 +1,14 @@
 package com.toko.maju.domains.v1;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -25,6 +32,10 @@ public class Supplier extends BaseEntity {
 	private String phoneNumber;
 	private String bankAccount;
 	private String bankName;
+
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "supplier")
+	private Set<Product> products = new HashSet<Product>();
 
 	@Builder
 	public Supplier(Long id, String name, String code, String address, String phoneNumber, String bankAccount,
